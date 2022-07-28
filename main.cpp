1835aedb0fe0f8e3e33103ff105a9c73a344c28e
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2022-07-28 15:28:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// TODO: 
-// spacebar: play simulation, escape: pause simulation
 #include "sim/Utils.hpp"
 
 using namespace sf;
@@ -52,14 +50,6 @@ int main()
     {
         Clear(window);
         Update(charges, t,dt);
-        // for (auto& charge : charges) {
-        //     if (charge.kinematics.velocity.magnitude() < 10)
-        //     for (auto& tp : charge.trail)
-        //     if (tp->age > tp->lifetime/1.5f)
-        //     tp->age = tp->lifetime;
-        // }
-        
-        
 
         if (showing_particles)
         Draw(charges, window);
@@ -71,63 +61,13 @@ int main()
         if (counting_particles)
         CountParticles(charges, window);
 
-
-        // for (auto& charge : charges)
-        // if (charge.kinematics.velocity.magnitude() > 200.f) {
-        //     // charge.charge *= -1;
-        //     // charge.kinematics.velocity = charge.kinematics.velocity.randomize().normalize() * 100.f;
-        //     // if (n % 2 == 0)
-        //     //      charge.kinematics.velocity = charge.kinematics.velocity.normal().randomize().normalize() * 100.f;
-        //     // else charge.kinematics.velocity = charge.kinematics.velocity.tangent().randomize().normalize() * 100.f;
-        //     // charge.kinematics.velocity = charge.kinematics.velocity.normal().randomize().normalize() * 100.f;
-        //     charge.kinematics.velocity *= Random();
-        //     // charge.kinematics.velocity = charge.kinematics.velocity.randomize() / 2.f;
-        //     // std::cout << "Acceleration: " << charge.kinematics.acceleration.magnitude() << std::endl;
-        // }
-        
-        // for (int i = 0; i < charges.size(); i++) {
-        //     // compute the distance between the i'th charge and all other charges
-        //     for (int j = i + 1; j < charges.size(); j++) {
-        //         float distance = charges[i].DistanceTo(charges[j]);
-        //         float angular_velocity1 = charges[i].kinematics.velocity.magnitude() / distance;
-        //         float angular_velocity2 = charges[j].kinematics.velocity.magnitude() / distance;
-        //         if (n % 2 == 0) {
-        //             std::cout << std::endl << "Angular velocities: " << angular_velocity1 << " , " << angular_velocity2 << std::endl;
-        //             std::cout << "Actual angular velocities: " << charges[i].kinematics.angular_velocity << " , " << charges[j].kinematics.angular_velocity << std::endl;
-        //         }
-                
-        //         // if (distance < charges[i].radius + charges[j].radius) {
-
-        //         // }
-        //         // std::cout << "Too close of an orbit: " << distance << std::endl;
-        //     }
-        // }
-
-
         /* Events */
         HandleInputEvents(charges, window, events);
         if (events.EscapePressed()) PauseSimulation(charges, window, events);
 
 
-        // if (n % (FPS/PLOT_SPEED) == 0)
-        // {
-        //     WriteToFile(n/8,
-        //     TotalKineticEnergy(charges),
-        //     TotalPotentialEnergy(charges));
-        // }
-
-
-        // test_particle.MoveCenterTo(CENTER + Vec2D(r*cos_theta, r*sin_theta));
-        // test_particle.Draw(window);
-
-
-
         n++;
         t += dt;
-        // theta = fmod
-        // (((float(n)/float(FPS))*2*PI), (2*PI));
-        // cos_theta = cos(theta);
-        // sin_theta = sin(theta);
         window.display();
     }
 
